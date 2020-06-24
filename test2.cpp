63a502ac623bda53881b870fb601d6e96862ca5f
Trafalgar99 <1286015267@qq.com>
2020-06-24 09:43:18 +0800

    6.24

--- test2.cpp
+++ test2.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
+#include <array>
+#include <deque>
+#include <list>
 using namespace std;
 
 int main(void)
 {
     istringstream a;
+    array<int, 3> a{1, 2, 3};
+    list<deque<int>> ad;
     return 0;
 }
