fe3b90841af389ec74edf2179163ba5a32a2935a
Trafalgar99 <1286015267@qq.com>
2020-07-07 21:39:26 +0800

    7.7

--- test2.cpp
+++ test2.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <array>
 #include <deque>
+#include <stack>
+#include <forward_list>
 #include <list>
 using namespace std;
 
 int main(void)
 {
-    istringstream a;
-    array<int, 3> a{1, 2, 3};
-    list<deque<int>> ad;
+    // ifstream in("./s.txt", ifstream::in);
+    // deque<int> a;
+    // forward_list<int> aa;
+    // array<int, 5> vf{1, 2, 3, 4, 5};
+    // for (auto i : vf)
+    // {
+    //     cout << i << endl;
+    // }
+    // auto op = aa.begin();
+    // op--;
+
+    // list<int> as;
+    // auto op = as.begin() - as.end();
+
+    // list<int> ass;
+    // vector<double> x(ass.begin(), ass.end());
+    // // ass.assign();
+
+    // string sd;
+    // deque<string> wq;
+    // wq.push_back(sd);
+    // list<string> la{"1", "2", "3"};
+
+    // for (auto i : la)
+    // {
+    //     cout << i << endl;
+    // }
+    // stack<string,vector<string>> s{wq};
+    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    auto p1 = a;
+    auto p2 = *(a + 1)[1];
+    cout << p2 << endl;
+    cout << (a + 1) << endl;
     return 0;
 }
